eb4f02f97e3b476db166f6fad8559eb1f91be78d
Abudew <64744776+Abudew@users.noreply.github.com>
2022-03-17 15:42:42 +0100

    Create index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://api-ap-herokuuu.herokuapp.com/'
+        origin: 'https://cocky-snyder-5a02cf.netlify.app'
     })
 );
 app.options('*', cors());
